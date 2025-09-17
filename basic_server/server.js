c4eada6dc77bca818753601704cc9f09b72bca8f
Abinash Sahoo <implabinash@gmail.com>
2025-09-17 13:30:07 +0530

    feat: basic form handling

--- basic_server/server.js
+++ basic_server/server.js
@@ -1,7 +1,26 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+    if (req.url == "/") {
+        res.write("<html>");
+        res.write("<header><title>Demo App</title></header>");
+        res.write(
+            '<body><form method="POST" action="/message"><input type="text" name="message"/> <button type="submit">Send</button></form></body>',
+        );
+        res.write("</html>");
+
+        return res.end();
+    }
+
+    if (req.url === "/message" && req.method === "POST") {
+        fs.writeFileSync("message.txt", "Success");
+
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+
+        return res.end();
+    }
 
     res.setHeader("Content-Type", "text/html");
 
