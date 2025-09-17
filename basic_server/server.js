d4d523032f00d39462ef37c41006943c5ffc76e5
Abinash Sahoo <implabinash@gmail.com>
2025-09-17 13:15:11 +0530

    fix: send response back

--- basic_server/server.js
+++ basic_server/server.js
@@ -1,7 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method);
+
+    res.setHeader("Content-Type", "text/html");
+
+    res.write("<html>");
+    res.write("<header><title>Demo App</title></header>");
+    res.write("<body><h1>Hello world</h1></body>");
+    res.write("</html>");
+
+    res.end();
 });
 
 server.listen(3000);
