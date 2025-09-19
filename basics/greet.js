8b9ae36047adcc843f84754041350b93a1fc87c1
Abinash Sahoo <implabinash@gmail.com>
2025-09-19 12:11:31 +0530

    feat: add basic cli greeting tool

---
+++ basics/greet.js
@@ -0,0 +1,17 @@
+const name = process.argv[2];
+
+const hour = new Date().getHours();
+
+const greet = (hour) => {
+    if (hour > 6 && hour < 12) {
+        return "Good Morning";
+    } else if (hour < 16) {
+        return "Good Afternoon";
+    } else if (hour < 20) {
+        return "Good Evening";
+    } else {
+        return "Good Night";
+    }
+};
+
+console.log(`${greet(hour)}, ${name}`);
